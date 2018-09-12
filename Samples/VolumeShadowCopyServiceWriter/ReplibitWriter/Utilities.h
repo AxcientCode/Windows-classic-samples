e4a34366136e16037c18623a82d1609849a94cab
diazfran <diaz.fran@gmail.com>
2018-09-12 12:30:13 -0300

    Change ReplibitWriter to use standard containers and CChunkBitmap.

--- Samples/VolumeShadowCopyServiceWriter/ReplibitWriter/Utilities.h
+++ Samples/VolumeShadowCopyServiceWriter/ReplibitWriter/Utilities.h
@@ -3,6 +3,15 @@
 #ifndef _UTILITIES_H
 #define _UTILITIES_H
 
-bool GetNTFSvolumeData(HANDLE m_hVolume, NTFS_VOLUME_DATA_BUFFER& m_nvdbData);
+#include <string>
+#include <ChunkBitmap.h>
+
+bool SetSparseFlag(HANDLE hFile, BOOL bSetSparse);
+bool TruncateFile(HANDLE hFile);
+bool MoveFilePointer(HANDLE hFile, LONG lPosition);
+HANDLE CreateFileInDevice(DWORD dwDispotision, const std::wstring &wsDevice, const wchar_t *pwcFileName);
+void GetFullPath(_In_ const std::wstring &wsDevice, _In_ const wchar_t *pwcFileName, _Out_ std::wstring &pwcFullPath);
+bool GetAllocationBitmap(HANDLE hVolume, CChunkBitmap *pBitmap, CHUNK_BITMAP_SET_STATE iBitmapSetState);
+bool GetNTFSvolumeData(HANDLE m_hVolume, NTFS_VOLUME_DATA_BUFFER &m_nvdbData);
 
 #endif
