a7ec7d12bf944e7e0454d1e4d299acbeaeb0f7f9
azharkov <azharkov@axcient.com>
2022-05-17 18:27:55 +0300

    [RA-3161] added cmd to set unlimited storage area for a volume to efsvss_2003

--- Samples/vshadow/src/shadow.h
+++ Samples/vshadow/src/shadow.h
@@ -19,6 +19,7 @@
 #include "vss.h"
 #include "vswriter.h"
 #include "vsbackup.h"
+#include <vsmgmt.h>
 
 // VDS includes
 #include "vds.h"
@@ -30,6 +31,8 @@
 #include "vssclient.h"
 
 
+// Software provider GUID {b5946137-7b9f-4925-af80-51abd60b20d5}
+const GUID VSS_SWPRV_ProviderId = { 0xb5946137, 0x7b9f, 0x4925, { 0xaf, 0x80, 0x51, 0xab, 0xd6, 0xb, 0x20, 0xd5 } };
 
 
 /////////////////////////////////////////////////////////////////////////
